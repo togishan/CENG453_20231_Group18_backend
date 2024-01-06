efc3f7a962332c0281cda116e0520d11c174dd3e
Oğuzhan Taşkın <105604998+togishan@users.noreply.github.com>
2024-01-07 01:37:30 +0300

    game Repository test (under construction)

--- src/test/java/CENG453/group18/repository/GameRepositoryTest.java
+++ src/test/java/CENG453/group18/repository/GameRepositoryTest.java
@@ -2,6 +2,7 @@ package CENG453.group18.repository;
 
 import CENG453.group18.entity.Game;
 
+import CENG453.group18.entity.Player;
 import CENG453.group18.enums.GameType;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.MethodOrderer;
@@ -22,77 +23,73 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class GameRepositoryTest {
     @Autowired
+    private PlayerRepository playerRepository;
+    @Autowired
     private GameRepository gameRepository;
-    private final Game game = new Game(null, null, null, null, GameType.SinglePlayer);
+    private Game game;
+    private Integer gameID;
+    private String appropriateName1 = "oguzhancsacsasc22332";
+    private String appropriateEmail1 = "oguzhanvdsdvsa22332@metu.edu.tr";
 
-    private  Integer gameID;
-
+    private Player player;
     @Test
     @Order(1)
     @Rollback(false)
+    void deleteUsers()
+    {
+        playerRepository.deletePlayerByUsername(appropriateName1);
+    }
+    @Test
+    @Order(1)
+    @Rollback(false)
+    void registerGameHost()
+    {
+        Player player = new Player();
+        player.setUsername(appropriateName1);
+        player.setEmail(appropriateEmail1);
+        player.setPassword("123");
+        playerRepository.save(player);
+
+    }
+    @Test
+    @Order(2)
+    @Rollback(false)
     void initializeRepository()
     {
-        gameRepository.save(game);
-        gameID = game.getGameID();
+        game = new Game(player, null, null, null, GameType.SinglePlayer);
+        gameID = gameRepository.save(game).getGameID();
+        System.out.println(gameID);
     }
 
     @Test
-    @Order(2)
+    @Order(3)
     void testGetGameByID()
     {
         // test by not existing gameID
         Game notExistingGame = gameRepository.getGameByGameID(111111);
         assertNull(notExistingGame);
+        System.out.println(gameID);
         // test by existing gameID
         Game existingGame = gameRepository.getGameByGameID(gameID);
         assertNotNull(existingGame);
     }
-
     @Test
-    @Order(2)
-    void testFindPlayerByEmail()
+    @Order(4)
+    void testDeleteAndExistGameByGameID()
     {
-        Player existingPlayer = playerRepository.findPlayerByEmail("oguzhan@metu.edu.tr");
-        Player notExistingPlayer = playerRepository.findPlayerByEmail("oguz@metu.edu.tr");
-        assertEquals(existingPlayer, player);
-        assertNull(notExistingPlayer);
-    }
-
-    @Test
-    @Order(2)
-    void testFindPlayerBySessionKey()
-    {
-        Player existingPlayer = playerRepository.findPlayerBySessionKey("RANDOM_SESSION_KEY");
-        Player notExistingPlayer = playerRepository.findPlayerBySessionKey("KEY_SESSION_RANDOM");
-        assertEquals(existingPlayer, player);
-        assertNull(notExistingPlayer);
-    }
-
-    @Test
-    @Order(2)
-    void testFindPlayerByResetKey()
-    {
-        Player existingPlayer = playerRepository.findPlayerByResetKey("RANDOM_RESET_KEY");
-        Player notExistingPlayer = playerRepository.findPlayerByResetKey("KEY_RESET_RANDOM");
-        assertEquals(existingPlayer, player);
-        assertNull(notExistingPlayer);
+        // check game exists then delete the game and check again
+        boolean existingGame = gameRepository.existsGameByGameID(gameID);
+        assertTrue(existingGame);
+        gameRepository.deleteGameByGameID(gameID);
+        boolean notExistingGame = gameRepository.existsGameByGameID(gameID);
+        assertFalse(notExistingGame);
     }
     @Test
-    @Order(2)
-    void testExistsPlayerByUsername()
+    @Order(6)
+    @Rollback(false)
+    void destruct()
     {
-        boolean trueAssertion = playerRepository.existsPlayerByUsername("oguzhan");
-        boolean falseAssertion = playerRepository.existsPlayerByUsername("oguz");
-        assertTrue(trueAssertion);
-        assertFalse(falseAssertion);
-    }
-    @Test
-    @Order(2)
-    void testExistsPlayerByEmail()
-    {
-        boolean trueAssertion = playerRepository.existsPlayerByEmail("oguzhan@metu.edu.tr");
-        boolean falseAssertion = playerRepository.existsPlayerByEmail("oguz@metu.edu.tr");
-        assertTrue(trueAssertion);
-        assertFalse(falseAssertion);
+        gameRepository.deleteGameByGameID(gameID);
+        playerRepository.deletePlayerByUsername(appropriateName1);
     }
 }
